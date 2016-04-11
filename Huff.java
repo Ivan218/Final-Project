261aea24822afe7ab2c07c4a6a718bf8999b1f8d
Ivan218 <Ivan218@users.noreply.github.com>
2016-04-11 19:34:35 -0400

    Added files via upload

---
+++ Huff.java
@@ -0,0 +1,31 @@
+
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ *
+ *  @author ivanjrivera
+ *  @author Chris Maselli
+ *  @version Apr 11, 2016
+ */
+
+public class Huff
+{
+    // ----------------------------------------------------------
+    /**
+     * Most activity takes place here
+     * @param args default
+     */
+    public static void main(String[] args)
+    {
+        int bitmask = 0x000F;
+        int val = 0010;
+        //8^1
+        System.out.println(val);
+        val = 0020;
+        //8^1
+        System.out.println(val);
+
+    }
+}
